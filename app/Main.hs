4dfe48a9b83dd6ba86e9d21b2ecd8d130fc85120
James Butcher <jamesbutcher167@gmail.com>
2023-08-24 00:04:48 +0000

    Initial commit

---
+++ app/Main.hs
@@ -0,0 +1,20 @@
+module Main where
+
+import System.Environment
+import Data.Foldable (traverse_)
+import Data.List.Extra (snoc)
+
+data Token = Num Int | Add | Sub | Mul | Div
+
+split' :: Eq a => [a] -> a -> [a] -> [[a]]
+split' [] _ [] = []
+split' [] _ carry = [carry]
+split' (x:xs) d carry | x /= d = split' xs d (snoc carry x)
+                      | null carry = split' xs d []
+                      | otherwise = carry : split' xs d []
+
+main :: IO ()
+main = do
+  --args <- head <$> getArgs
+  putStrLn $ show $ split' "test a  b " ' ' []  
+
